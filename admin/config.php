6795adf71e99c9d6e0aa8cae427b45395727bbbd
Ana Isabel Palasi Lallana <anapalasi@ieshonorigarcia.net>
2021-12-03 11:30:18 +0100

    Conexion BD

---
+++ admin/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('RUTA', 'http://localhost/caritas/');
+ 
+// nombre de base de datos
+$bd_config = array(
+    'db_name' => 'caritas',
+    'user' => 'root',
+    'pass' => 'root12002661'    
+);
+
+?>
+
