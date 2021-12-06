96864a162b2a528b1b4eef43f60a8ff7d8ef3180
Anabel Palasí <anapalasi@ieshonorigarcia.net>
2021-12-06 10:32:58 +0100

    Actualitza items

--- viewCart.php
+++ viewCart.php
@@ -37,12 +37,12 @@ $cart = new Cart;
     function updateCartItem(obj,id){
 
         $.get("cartAction.php", {action:"updateCartItem", id:id, qty:obj.value}, function(data){
-	    if(data == 'ok'){
+	    //if(data == 'ok'){
                 location.reload();
-        }else{
+       //}else{
            
-            alert('No s\' ha pogut actualitzar');
-        }
+          // alert('No s\' ha pogut actualitzar');
+        //}
         });
     }
     </script>
